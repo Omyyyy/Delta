1bf2fb64dcaa885cd32158867e3c3ac181c1697f
Omar <omarmammadli.newschoolgeorgia@gmail.com>
2022-05-16 20:53:58 +0200

    input

--- basm.py
+++ basm.py
@@ -14,28 +14,26 @@ if argv != []:
         INFO = True
 
 filenamewithoutext = os.path.splitext(filename)[0]
-compstart = time.process_time()
+compstart = time.perf_counter()
 
 with open(filename) as f:
-    for i in f:
-        if i[0] == '#':
+    for code in f:
+        if code[0] == '#' or code.isspace():
             continue
-        else:
-            code = i
 
         code = code.strip()
 
-        compiler.Compiler(code, filename).basm2py()
+        compiler.Compiler(code, filename).basm2py() if not code.isspace() and code != "\n" else None
 
-compend = time.process_time()
+compend = time.perf_counter()
 
 print(f"\n[INFO]: Compiled {filename} in {round((compend - compstart)*1000, 2)}ms\n") if INFO else None
 
-runstart = time.process_time()
+runstart = time.perf_counter()
 
 try:
     exec(pycode.pycode)
-    runend = time.process_time()
+    runend = time.perf_counter()
     print(f"\n[INFO]: Run {filename} in {round((runend - runstart)*1000, 2)}ms") if INFO else None
 
 except Exception as e:
--- compiler.py
+++ compiler.py
@@ -3,16 +3,22 @@ import pycode
 class Compiler:
     def __init__(self, line: str, filename):
         self.line = line
-        self.cmd = line.split()[0]
-        self.args = [arg.strip() for arg in line.split(" ")[1:]]
+        self.cmd = line.split(" ", 1)[0]
+        self.args = [arg.strip() for arg in line.split(" ", 1)[1].split(",")]
         self.arglen = len(self.args)
         self.filename = filename
 
     def basm2py(self):
         if self.cmd == "print":
-            toprint = " ".join(self.args).removeprefix('"').removesuffix('"')
+            toprint = self.args[0]
             pycode.pycode += f"print('{toprint}')\n"
         
         elif self.cmd == "printf":
-            toprint = eval(" ".join(self.args))
-            pycode.pycode += f"print('{toprint}')\n"
+            evaled = str(eval(self.args[0]))
+            toprint = evaled if evaled[0] == '"' and evaled[-1] == '"' else '"' + evaled + '"'
+            pycode.pycode += f"print({toprint})\n"
+
+        elif self.cmd == "input":
+            prompt = self.args[0]
+            varname = self.args[1]
+            pycode.pycode += f"{varname} = input({prompt})\n"
